41c8617f44eecdf81c05f8861ed692b6165236d5
Paul Milbank <Paul Milbank>
2016-04-07 14:26:22 +1200

    Add set-passengerlane script to repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # poshtsa
+
+Feel free fork this and save yourself at least $47000 next time you want to andomly chose a lane for passengers.
---
+++ set-passengerlane.ps1
@@ -0,0 +1,45 @@
+﻿<#
+Written by Paul Milbank
+07/04/2016
+
+Script to randomly direct pasengers down one lane or another.
+#>
+
+function Print-Leftarrow ()
+{
+    $leftarrow = @("
+       /|----
+      /      |
+      \      |
+       \|----
+       ")
+Write-output $leftarrow
+}
+
+
+function Print-Rightarrow ()
+{
+    $rightarrow = @("
+      ----|\
+     |      \
+     |      /
+      ----|/
+       ")
+Write-output $rightarrow
+}
+
+do
+{
+    $random = Get-Random -Minimum 0 -Maximum 2
+    if ($random -eq "0")
+        {
+            Print-Leftarrow
+        }
+    else
+        {
+            Print-Rightarrow
+        }
+$anykey = Read-Host "Press`"enter`" to continue or `"q`" to quit"
+}
+until ($anykey -eq "q")
+
